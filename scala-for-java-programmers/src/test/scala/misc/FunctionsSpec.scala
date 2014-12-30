5d25d4b14ceee8429b35f2d579582b8398621eb1
David Dufour-Boivin <david.dufour-boivin@zalando.de>
2014-12-30 18:09:57 +0100

    Add tests for lazy values. Correct tests for variable arguments.

--- scala-for-java-programmers/src/test/scala/misc/FunctionsSpec.scala
+++ scala-for-java-programmers/src/test/scala/misc/FunctionsSpec.scala
@@ -1,10 +1,16 @@
 package misc
 
+import java.text.MessageFormat
+
 import org.specs2.mutable.Specification
 
 object FunctionsSpec extends Specification {
 
   "functions" should {
+
+    /*
+        SftI: 2.7 Functions
+     */
 
     "be specifiable without return type" in {
       def f(x: Int) = x + 1
@@ -32,6 +38,11 @@ object FunctionsSpec extends Specification {
       i must be equalTo 3
     }
 
+
+    /*
+        SftI: 2.8 Default and Named Arguments
+     */
+
     "be declared with default parameters" in {
       def decorate(str: String, left: String = "[", right: String = "]") = left + str + right
       decorate("asd", "123", "890") must be equalTo "123asd890"
@@ -40,6 +51,10 @@ object FunctionsSpec extends Specification {
       decorate("asd") must be equalTo "[asd]"
       decorate("asd", right = "r", left = "l") must be equalTo "lasdr"
     }
+
+    /*
+        SftI: 2.9 : Variable Arguments
+     */
 
     "be declared with variable arguments" in {
       def f(a: Int*): Int = a.length
@@ -63,6 +78,40 @@ object FunctionsSpec extends Specification {
 
       f(11, 12) must be equalTo 23
       f(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) must be equalTo 12
+    }
+
+    "be called with manual cast to 'object' when calling Java methods" in {
+      val s = MessageFormat.format(
+        "Formatting {0} and {1}.",
+        "first value",
+        42.asInstanceOf[AnyRef])
+      s must be equalTo "Formatting first value and 42."
+    }
+
+  }
+
+  /*
+      SftI: 2.11 : Lazy Values
+   */
+
+  "lazy values" should {
+
+    "just work" in {
+      var c = 0
+      def f(): Int = {
+        c += 1
+        c
+      }
+      lazy val v = f()
+
+      // 'v' was not yet evaluated, since its marked 'lazy'. 'c' is still 0.
+      c must be equalTo 0
+      // Checking 'v' will have the lazy initialization be performed, calling 'f' and incrementing 'c' in the process.
+      v must be equalTo 1
+      c must be equalTo 1
+      // Checking 'v' again should not have the initialization called a second time. Should still be equal to 1.
+      v must be equalTo 1
+      c must be equalTo 1
     }
 
   }
