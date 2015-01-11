503792e9751d2cf7c87e859a1f6626e16b7f01ec
Adeynack <david.dufour.boivin@gmail.com>
2015-01-12 00:45:45 +0100

    Progress in chapter 4.

--- scala-for-java-programmers/src/test/scala/ScalaForTheImpatient/Chapter04.scala
+++ scala-for-java-programmers/src/test/scala/ScalaForTheImpatient/Chapter04.scala
@@ -8,6 +8,7 @@
 package ScalaForTheImpatient
 
 import org.specs2.mutable.Specification
+
 import scala.collection._
 
 object Chapter04 extends Specification {
@@ -82,11 +83,13 @@ object Chapter04 extends Specification {
       val scores = Map(("Alice", 10), ("Bob", 3), ("Cindy", 8))
 
       val alice = scores.get("Alice")
+      alice must beAnInstanceOf[Option[Int]]
       alice.isDefined must beTrue
       alice.get must be equalTo 10
       alice must be equalTo Some(10)
 
       val xxxxx = scores.get("xxxxx")
+      xxxxx must beAnInstanceOf[Option[Int]]
       xxxxx.isDefined must beFalse
       xxxxx must beNone
       def getXxxxx = xxxxx.get
@@ -98,7 +101,235 @@ object Chapter04 extends Specification {
   //  4.3 Updating Map Values
   //
 
-//  "updating map values" should {
-//
-//  }
+  "updating map values" should {
+    "update or add using the parenthesis" in {
+      val scores = mutable.Map[String, Int]()
+      scores must have length 0
+
+      scores("Bob") = 10
+      scores must have length 1
+      scores("Bob") must be equalTo 10
+
+      scores("Fred") = 7
+      scores must have length 2
+      scores("Fred") must be equalTo 7
+    }
+  }
+
+  "add multiple associations using the += operator" in {
+    val scores = mutable.Map[String, Int]()
+    scores must have length 0
+
+    scores +=("Bob" -> 10, "Fred" -> 7)
+    scores must have length 2
+    scores("Bob") must be equalTo 10
+    scores("Fred") must be equalTo 7
+  }
+
+  "remove single association using the -= operator" in {
+    val scores = mutable.Map("Bob" -> 10, "Alice" -> 0, "Fred" -> 7)
+    scores must have length 3
+    scores must have key "Bob"
+    scores must have key "Alice"
+    scores must have key "Fred"
+
+    scores -= "Alice"
+    scores must have length 2
+    scores must have key "Bob"
+    scores must not haveKey "Alice"
+    scores must have key "Fred"
+  }
+
+  "adding an element to an immutable map returns a new map with the modification" in {
+    val scores = immutable.Map("Alice" -> 0)
+    scores must have length 1
+
+    val newScores = scores +("Bob" -> 10, "Fred" -> 7)
+    scores must have length 1
+    newScores must beAnInstanceOf[immutable.Map[String, Int]]
+    newScores must have length 3
+    newScores("Alice") must be equalTo 0
+    newScores("Bob") must be equalTo 10
+    newScores("Fred") must be equalTo 7
+
+    scores must not beTheSameAs newScores
+  }
+
+  "the same using the same identifier and the += operator" in {
+    var scores = immutable.Map("Alice" -> 0)
+    scores must have length 1
+
+    scores = scores + ("Bob" -> 10)
+    scores must have length 2
+    scores("Alice") must be equalTo 0
+    scores("Bob") must be equalTo 10
+
+    scores += ("Fred" -> 7)
+    scores must have length 3
+    scores("Alice") must be equalTo 0
+    scores("Fred") must be equalTo 7
+    scores("Bob") must be equalTo 10
+  }
+
+  "removing an element to an immutable map does the same" in {
+    val scores = immutable.Map("Alice" -> 0, "Bob" -> 10, "Fred" -> 7)
+    val newScores = scores - "Bob"
+    scores must not beTheSameAs newScores
+
+    scores must have length 3
+    scores("Alice") must be equalTo 0
+    scores("Bob") must be equalTo 10
+    scores("Fred") must be equalTo 7
+
+    newScores must have length 2
+    newScores("Alice") must be equalTo 0
+    newScores must not haveKey "Bob"
+    newScores("Fred") must be equalTo 7
+  }
+
+  //
+  //  4.4 Iterating over Maps
+  //
+
+  "iterating over Maps" should {
+
+    "with for-tuple syntax" in {
+      val scores = immutable.Map("Bob" -> 10, "Fred" -> 7, "Alice" -> 0)
+      val sb = new StringBuffer
+      for ((k, v) <- scores) sb append s"$k is $v years old. "
+      sb.toString must be equalTo "Bob is 10 years old. Fred is 7 years old. Alice is 0 years old. "
+    }
+
+    "obtain only keys" in {
+      val scores = immutable.Map("Bob" -> 10, "Fred" -> 7, "Alice" -> 0)
+      scores.keySet must contain(exactly("Bob", "Fred", "Alice"))
+    }
+
+    "obtain only values" in {
+      val scores = immutable.Map("Bob" -> 10, "Fred" -> 7, "Alice" -> 0)
+      scores.values must contain(exactly(10, 7, 0))
+    }
+
+    "be used to simply reverse a map (keys become values and vice versa)" in {
+      val scores = immutable.Map("Bob" -> 10, "Fred" -> 7, "Alice" -> 0)
+      val reversed = for ((k, v) <- scores) yield (v, k)
+      reversed must beAnInstanceOf[immutable.Map[Int, String]]
+      reversed must have length 3
+      reversed(10) must be equalTo "Bob"
+      reversed(7) must be equalTo "Fred"
+      reversed(0) must be equalTo "Alice"
+    }
+
+    "reverse a map with duplicate values" in {
+      // WARNING: Reversing from a map with duplicate values will only retain the last tuple.
+      val scores = immutable.Map("Bob" -> 10, "Fred" -> 7, "Alice" -> 10)
+      val reversed = for ((k, v) <- scores) yield (v, k)
+      reversed must beAnInstanceOf[immutable.Map[Int, String]]
+      // ("Bob", 10) was overwriten by ("Alice", 10).
+      // ("Fred", 7) stayed.
+      reversed must have length 2
+      reversed(10) must be equalTo "Alice"
+      reversed(7) must be equalTo "Fred"
+    }
+  }
+
+  //
+  //  4.5 Sorted Maps
+  //
+
+  "sorted maps" should {
+
+    "be constructed specifying SortedMap (which is a tree map)" in {
+      val scores = immutable.SortedMap("Alice" -> 10, "Fred" -> 7, "Bob" -> 3, "Cindy" -> 8)
+      scores.keySet must contain(exactly("Alice", "Bob", "Cindy", "Fred")).inOrder
+    }
+
+    "for mutable tree map, use Java's TreeMap" in {
+      val scores = new java.util.TreeMap[String, Int]
+      scores put("Alice", 10)
+      scores put("Fred", 7)
+      scores put("Bob", 3)
+      scores put("Cindy", 8)
+      scores.keySet.toArray must be equalTo Array("Alice", "Bob", "Cindy", "Fred")
+    }
+
+    "for mutable ordered hash maps, use LinkedHashMap" in {
+      val scores = mutable.LinkedHashMap("Alice" -> 10, "Fred" -> 7, "Bob" -> 3, "Cindy" -> 8)
+      scores.keySet must contain(exactly("Alice", "Fred", "Bob", "Cindy")).inOrder
+    }
+  }
+
+  //
+  //  4.6 Interoperating with Java
+  //
+
+  "inter operating with Java" should {
+
+    "be done on mutable maps" in {
+      import scala.collection.JavaConversions.mapAsScalaMap
+      val scoresJava = new java.util.TreeMap[String, Int]
+      val scoresScala: mutable.Map[String, Int] = scoresJava
+
+      scoresJava size() must be equalTo 0
+      scoresScala must have length 0
+
+      scoresJava.put("Alice", 9)
+      scoresJava size() must be equalTo 1
+      scoresJava.get("Alice") must be equalTo 9
+      scoresScala must have length 1
+      scoresScala("Alice") must be equalTo 9
+
+      // adding via method "put"
+      scoresScala put("Bob", 11)
+      scoresScala must have length 2
+      scoresScala("Bob") must be equalTo 11
+      scoresJava size() must be equalTo 2
+      scoresJava.get("Bob") must be equalTo 11
+
+      // adding via operator +=
+      scoresScala += "Fred" -> 7
+      scoresScala must have length 3
+      scoresScala("Fred") must be equalTo 7
+      scoresJava size() must be equalTo 3
+      scoresJava.get("Fred") must be equalTo 7
+    }
+
+    "be done on property maps" in {
+      import scala.collection.JavaConversions.propertiesAsScalaMap
+      val javaProps = System.getProperties
+      javaProps must beAnInstanceOf[java.util.Properties]
+
+      val scalaProps: collection.Map[String, String] = javaProps
+
+      scalaProps must have length javaProps.size()
+
+      //      for ((k, v) <- scalaProps) {
+      //        s"both must have the same value for key $k" in {
+      //          javaProps.get(k) must be equalTo v
+      //        }
+      //      }
+
+      //      scalaProps.keySet foreach { k =>
+      //        scalaProps(k) must be equalTo javaProps.get(k).toString()
+      //      }
+
+      // todo: Tried to do test in loops as described in http://stackoverflow.com/questions/6805267/scalatest-or-specs2-with-multiple-test-cases
+      //       but it didn't work.
+    }
+
+    "pass a Scala map to a Java method" in {
+      import scala.collection.JavaConversions.mapAsJavaMap
+      import java.awt.font.TextAttribute._ // import the different values of the enum
+      val attrs = Map(FAMILY -> "Serif", SIZE -> 12) // a Scala map
+      val font = new java.awt.Font(attrs) // expects a Java map
+      font.getFamily must be equalTo "Serif"
+      font.getSize must be equalTo 12
+    }
+
+    //
+    // 4.7 Tuples
+    //
+
+    
+  }
 }
