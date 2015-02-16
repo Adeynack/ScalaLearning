fb38ffeedadab7648d449de0dc1e3fe75879bb5f
David Dufour-Boivin <david.dufour-boivin@zalando.de>
2015-02-16 12:05:57 +0100

    More future and try specs.

--- scala-for-java-programmers/src/test/scala/misc/FutureAndTrySpec.scala
+++ scala-for-java-programmers/src/test/scala/misc/FutureAndTrySpec.scala
@@ -11,6 +11,8 @@ import scala.util.{Failure, Try}
 class FutureAndTrySpec extends Specification {
   sequential
 
+  def awaitResult[T](f: Future[T]): T = Await.result(f, Duration(10, SECONDS))
+
   "A Future depending on a Try" should {
     sequential
 
@@ -27,7 +29,7 @@ class FutureAndTrySpec extends Specification {
       }
 
       val resultFuture: Future[Int] = service()
-      val resultValue = Await.result(resultFuture, Duration(1, MINUTES))
+      val resultValue = awaitResult(resultFuture)
 
       resultValue must be equalTo 42
     }
@@ -47,7 +49,7 @@ class FutureAndTrySpec extends Specification {
       val resultFuture: Future[Int] = service()
       Await.ready(resultFuture, Duration(1, MINUTES))
 
-      def getValue = Await.result(resultFuture, Duration(1, MINUTES))
+      def getValue = awaitResult(resultFuture)
 
       getValue must throwA[ArithmeticException]
     }
@@ -66,7 +68,7 @@ class FutureAndTrySpec extends Specification {
 
       val resultFuture: Future[Int] = service()
       Await.ready(resultFuture, Duration(1, MINUTES))
-      def getValue = Await.result(resultFuture, Duration(1, MINUTES))
+      def getValue = awaitResult(resultFuture)
       getValue must throwA[IllegalStateException]("We were not ready for this...")
     }
 
@@ -95,7 +97,7 @@ class FutureAndTrySpec extends Specification {
       }
 
       val resultFuture: Future[Int] = service()
-      val resultValue = Await.result(resultFuture, Duration(1, MINUTES))
+      val resultValue = awaitResult(resultFuture)
 
       resultValue must be equalTo 42
     }
@@ -126,7 +128,7 @@ class FutureAndTrySpec extends Specification {
 
       val resultFuture: Future[Int] = service()
       Await.ready(resultFuture, Duration(1, MINUTES))
-      def getValue = Await.result(resultFuture, Duration(1, MINUTES))
+      def getValue = awaitResult(resultFuture)
       getValue must throwA[IllegalStateException]("Ach Mensch...")
     }
 
@@ -139,7 +141,7 @@ class FutureAndTrySpec extends Specification {
         82
       }
 
-      lazy val dependency2: Try[Int] =  {
+      lazy val dependency2: Try[Int] = {
         dependenciesTested += 1
         Failure(new IllegalStateException("Blabla"))
       }
@@ -161,10 +163,112 @@ class FutureAndTrySpec extends Specification {
 
       val resultFuture: Future[Int] = service()
       Await.ready(resultFuture, Duration(1, MINUTES))
-      def getValue = Await.result(resultFuture, Duration(1, MINUTES))
+      def getValue = awaitResult(resultFuture)
       getValue must throwA[IllegalStateException]("Blabla")
 
       dependenciesTested must be equalTo 2
+    }
+  }
+
+  "Chains of futures" should {
+
+    case class Value[T](value: T, id: String)
+
+    class Service() {
+      def listOfParentElements: Seq[Value[Int]] =
+        for (i <- 1 to 3) yield Value(i, i.toString)
+
+      def listOfSecondElements(parentElementId: String): Seq[Value[String]] =
+        for (i <- 1 to 5) yield Value(s"Second #$i for parent $parentElementId.", s"s$i")
+
+      def somethingWithFirstAndSecondId(firstId: String, secondId: String): Seq[Value[String]] =
+        for (i <- 1 to 10) yield Value(s"Something with '$firstId' and '$secondId'.", s"f $firstId of $secondId")
+    }
+
+    val service = new Service()
+
+    def use[TReturn](action: Service => TReturn): Future[TReturn] = {
+      val result: TReturn = action(service)
+      Future.successful(result)
+    }
+
+    "work with some 1-generic converter" in {
+      sequential
+
+      def firstOf[T](seq: Seq[T]): Future[T] =
+        seq.headOption match {
+          case Some(value) => Future.successful(value)
+          case _ => Future.failed(new NoSuchElementException("No element found."))
+        }
+
+      val finalAnswerFuture: Future[String] =
+        for (
+          accounts <- use(_.listOfParentElements);
+          firstOfFirstElement <- firstOf(accounts);
+          firstOfFirstId = firstOfFirstElement.id;
+          secondElements <- use(_.listOfSecondElements(firstOfFirstId));
+          firstOfSecondElement <- firstOf(secondElements);
+          firstOfSecondId = firstOfSecondElement.id;
+          finalResults <- use(_.somethingWithFirstAndSecondId(firstOfFirstId, firstOfSecondId));
+          firstOfFinalResult <- firstOf(finalResults)
+        ) yield firstOfFinalResult.id
+
+      val finalAnswer = awaitResult(finalAnswerFuture)
+      finalAnswer must be equalTo "f 1 of s1"
+    }
+
+//    "work with some 2-generics converter" in {
+//
+//      def withFirst[TIn, TOut](seq: Seq[TIn], f: TIn => TOut): Future[TOut] =
+//        seq.headOption match {
+//          case Some(value) => Future.successful(f(value))
+//          case _ => Future.failed(new NoSuchElementException("No element found."))
+//        }
+//
+//      // Simple example
+//
+////      def firstList = Future.successful(Seq(1, 2, 3, 4, 5))
+////
+////      val firstToString1: String = awaitResult(withFirst(awaitResult(firstList), _.toString))
+////
+////      val firstToString2: String =
+////        awaitResult(
+////          for (
+////            e <- firstList;
+////            s <- withFirst(e, _.toString);
+////          ) yield s
+////        )
+////
+////      firstToString1 must be equalTo "1"
+////      firstToString2 must be equalTo "1"
+//
+//      // Complex example
+//
+//      val finalAnswerFuture: Future[String] =
+//        for (
+//          accounts <- use(_.listOfParentElements);
+//          firstOfFirstId <- withFirst[Value[Int], String](accounts, _.id);
+//          secondElements <- use(_.listOfSecondElements(firstOfFirstId));
+//          firstOfSecondId <- withFirst[Value[String], String](secondElements, _.id);
+//          finalResults <- use(_.somethingWithFirstAndSecondId(firstOfFirstId, firstOfSecondId));
+//          finalId <- withFirst[Value[String], String](finalResults, _.id)
+//        ) yield finalId
+//
+//      val finalAnswer = Await.result(finalAnswerFuture, Duration(10, SECONDS))
+//      finalAnswer must be equalTo "f 1 of s1"
+//    }
+
+    "asd" in {
+
+      val e: Either[Int, Throwable] = Left(42)
+      e match {
+        case Left(i) => println(i)
+        case Right(e: NullPointerException) => println("null")
+        case Right(e: IllegalStateException) => println("illegal")
+        case Right(e: Throwable) => println("whatever else...")
+      }
+
+      true must beTrue
     }
   }
 }
